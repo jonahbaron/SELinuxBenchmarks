a4463e9978deb4009a18a4548a44b4b16c939056
Jonah Baron <jonahbaron0135@gmail.com>
2015-11-25 13:01:52 -0500

    Initial commit

---
+++ scanner.py
@@ -0,0 +1,94 @@
+#!/usr/bin/python
+
+import sys
+import subprocess
+import os
+from os import listdir
+
+
+def getHashes(hashfile):
+	with open(hashfile) as hfile:
+		hashcontent = hfile.readlines()
+
+	hashes = []
+
+	for line in hashcontent:
+		hashes.append(line.split(None, 1)[0])
+
+	return hashes
+
+def getFiles(directory):
+	files = []
+
+	for path, subdirs, localfiles in os.walk(directory):
+		for filename in localfiles:
+			f = os.path.join(path, filename)
+			files.append(f)
+
+	return files
+
+def checkHash(hashes,files):
+	print "Checking hashes:"
+
+	badFiles = []
+
+	for h in hashes:
+		print "Comparing files against hash - ", h
+		print ""
+
+		for f in files:
+			command = "md5sum " + f + " | awk '{ print $1 }'"
+			fileHash = subprocess.check_output(command, shell=True)
+			#fileHash = os.popen("md5sum " + f + " | awk '{ print $1 }'").read()
+			fileHash = fileHash.rstrip()
+			print fileHash
+			if h == fileHash:
+				print "MALICIOUS FILE DETECTED - ", f
+				badFiles.append(f)
+	print ""
+
+	return badFiles
+
+def main():
+	print "Malware scanner"
+	print ""
+	print "Number of args: ", len(sys.argv)
+
+	if len(sys.argv) < 2:
+		print "Error - usage: scanner.py hashes [directory]"
+		sys.exit(0)
+
+	hashfile = sys.argv[1]
+	directory = os.getcwd()
+
+	if len(sys.argv) == 3:
+		directory = sys.argv[2]
+
+	print "Arguments: ", hashfile, directory
+	print ""
+
+
+
+	hashes = getHashes(hashfile)
+
+	print "Hashes:"
+	for line in hashes:
+		print line
+	print ""
+
+	files = getFiles(directory)
+
+	print "Files:"
+	for f in files:
+		print f
+	print ""
+
+	badFiles = checkHash(hashes,files)
+
+	print "Malicious files:"
+	for f in badFiles:
+		print f
+	print ""
+
+if __name__ == "__main__":
+	main()
